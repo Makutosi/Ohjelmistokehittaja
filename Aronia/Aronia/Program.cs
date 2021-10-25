d7a5fc855138104b30d4a9576fb53e09cfe25ce2
unknown <eriko.korhonen@edu.keuda.fi>
2021-10-25 11:31:13 +0300

    harjoitus

---
+++ Aronia/Aronia/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aronia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hyvää päivää!");
+        }
+    }
+}
+
+namespace Aronia
+{
+    class OmaNimi
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eriko Korhonen");
+        }
+    }
+}
+
+namespace Aronia
+{
+    class YksiNumerot
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1,11,101,1001");
+        }
+    }
+}
+
+
