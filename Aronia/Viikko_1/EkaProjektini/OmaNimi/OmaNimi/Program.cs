7b03e2d66131463695253225e1d3fe9f106843eb
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-10-31 17:06:14 +0200

    Viikko_1/ OmaNimi/ 25.10.2021
    OmaNimi

---
+++ Aronia/Viikko_1/EkaProjektini/OmaNimi/OmaNimi/Program.cs
@@ -0,0 +1,13 @@
+﻿// Dia 9_Harjoituksia_Tehtävä 4/ 25.10.2021
+using System;
+
+namespace OmaNimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eriko Korhonen");
+        }
+    }
+}
