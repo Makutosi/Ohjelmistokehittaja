bfa2cc14b108c968e6fd26ffd0ede53589d887c6
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-10-31 17:13:25 +0200

    Viikko_1/ Tervehdys/ 25.10.2021
    Tervehdys

---
+++ Aronia/Viikko_1/EkaProjektini/Tervehdys/Tervehdys/Program.cs
@@ -0,0 +1,20 @@
+﻿// Dia 9_Harjoituksia_Tehtävä 6/ 25.10.2021
+using System;
+
+namespace Tervehdys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ole hyvä, ja anna sinun etunimesi: ");
+            string nimi = Console.ReadLine();
+            Console.WriteLine("Hei " + nimi + "!");
+
+            //
+            Console.WriteLine("\nMikä sinun nimesi on?");
+            string etunimi = Console.ReadLine();
+            Console.WriteLine("Tervetuloa " + etunimi + "!");
+        }
+    }
+}
