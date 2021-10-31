35dee5b65d64f041f14829dd1882b1c6e7e4add4
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-10-31 23:24:18 +0200

    Viikko_1/ Laske / 30.10.2021
    Laske

---
+++ Aronia/Viikko_1/EkaProjektini/Laske/Laske/Program.cs
@@ -0,0 +1,87 @@
+﻿// Dia 16_Harjoituksia/ 25.10.2021
+using System;
+
+namespace Laske
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Artimeettiset operaattorit
+            //1.
+           /* Console.WriteLine("Anna ensimmäinen luku: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            luku2 += 3;
+            Console.WriteLine(luku2);*/
+
+            //2.
+           /* Console.WriteLine("Anna ensimmäinen luku: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            luku2 -= 2;
+            Console.WriteLine(luku2);*/
+
+            //3.
+            /*Console.WriteLine("Anna ensimmäinen luku: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            luku2 *= 5;
+            Console.WriteLine(luku2);*/
+
+            //4.
+            /*Console.WriteLine("Anna ensimmäinen luku: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            luku2= luku1/luku2;
+            Console.WriteLine(luku2);*/
+
+            //5.
+           /* Console.WriteLine("Anna ensimmäinen luku: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            luku2 = luku1 % luku2;
+            Console.WriteLine(luku2);*/
+
+            //6.
+            /*Console.WriteLine("Anna ensimmäinen luku: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            luku1 +=luku2;
+            Console.WriteLine(luku1);*/
+
+            //7.
+            /*Console.WriteLine("Anna ensimmäinen luku: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            luku1 -=luku2;
+            Console.WriteLine(luku1);*/
+
+            //8.
+            /*Console.WriteLine("Anna ensimmäinen luku: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            luku1 *= luku2 * 5;
+            Console.WriteLine(luku1);*/
+
+            //9.
+            Console.WriteLine("Anna ensimmäinen luku: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            luku1 /= luku2;
+            Console.WriteLine(luku1);
+
+
+        }
+    }
+}
+
