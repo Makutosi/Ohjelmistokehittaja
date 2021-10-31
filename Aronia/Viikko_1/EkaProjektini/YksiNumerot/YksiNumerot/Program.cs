94198e0876b04f6bf61dd87b369f6871683c43fa
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-10-31 17:15:08 +0200

    Viikko_1/ YksiNumerot/ 25.10.2021
    YksiNumerot

---
+++ Aronia/Viikko_1/EkaProjektini/YksiNumerot/YksiNumerot/Program.cs
@@ -0,0 +1,17 @@
+﻿// Dia 9_Harjoituksia_Tehtävä 5/ 25.10.2021
+using System;
+
+namespace YksiNumerot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WriteLine(kirjoittaa ja vaihtaa rivin)
+            Console.WriteLine("1");
+            Console.WriteLine("11");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+        }
+    }
+}
