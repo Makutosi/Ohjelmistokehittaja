957dccd650ace5c5973e476015eb513bc5658dc1
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-10-31 17:01:31 +0200

    Viikko_1/ EkaProjektini/ 25.10.2021
    EkaProjektini

---
+++ Aronia/Viikko_1/EkaProjektini/EkaProjektini/Program.cs
@@ -0,0 +1,15 @@
+﻿// Dia 9_Harjoituksia_Tehtävä 3/ 25.10.2021
+using System;
+
+namespace EkaProjektini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hyvää päivää!");
+            
+        }
+    }
+}
