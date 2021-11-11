a47ad08c33a7d9aca0823ed88f6f5ebcbda28dab
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-11-11 19:40:15 +0200

    Osa 4: Käyttäjän syötteen lukeminen/ 7.11.2021
    Osa 4: Käyttäjän syötteen lukeminen/ 7.11.2021

---
+++ Aronia/C#_perusteet/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿// C#_perusteet/ Osa 4: Käyttäjän syötteen lukeminen/ 6.11.2021
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nimi; // alustetaan muuttujan nimi
+            int ika;
+
+            Console.Write("Anna nimesi: ");
+            nimi = Console.ReadLine();
+
+            Console.Write("Anna ikäsi: ");
+            ika = int.Parse(Console.ReadLine()); // muuttaa tekstijono numeroksi
+            // ika = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Nimesi on " + nimi + " ja olet " + ika + "vuotias!");
+            ika++;
+            Console.WriteLine("Vuoden päästä olet " + ika + " vuotias!");
+        }
+    }
+}
