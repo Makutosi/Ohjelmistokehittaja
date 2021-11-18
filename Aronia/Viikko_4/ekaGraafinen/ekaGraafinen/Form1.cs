c2e71c36c72afd681af7c8b658bfdbc5fde6dfed
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-11-18 20:00:47 +0200

    Viikko_4/ ekaGraafinen/ 15.11.2021
    Viikko_4/ Graafinen/ 15.11.2021

---
+++ Aronia/Viikko_4/ekaGraafinen/ekaGraafinen/Form1.cs
@@ -0,0 +1,26 @@
+﻿// 15.11.2021
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ekaGraafinen
+{
+    public partial class LomakeFM : Form
+    {
+        public LomakeFM()
+        {
+            InitializeComponent();
+        }
+
+        private void PainikeBT_Click(object sender, EventArgs e)
+        {
+            OtsikkoLB.Text = "Heippa maailma";
+        }
+    }
+}
