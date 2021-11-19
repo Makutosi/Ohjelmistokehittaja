3ad25e5164c2f4d64d5ab21e86d5fcaf306dc08b
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-11-19 13:39:47 +0200

    Harjoitus 2/ Ominaisuuksien lisäys/ 19.11.2021
    Harjoitus 2/ Lisätään ominaisuuksia/ 19.11.2021

---
+++ Aronia/Viikko_4/Harjoitus 2/Harjoitus 2/Form1.cs
@@ -0,0 +1,37 @@
+﻿// Viikko 4/ Harjoitus 2/ Lisätään ominaisuuksia/ 19.11.2021
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harjoitus_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TulostaBT_Click(object sender, EventArgs e)
+        {
+            // Määritellään teksti-muuttuja,string
+            // Annetaan nimi, teksti
+            // Haetaan ViestiTB:en sisällö, ViestiTB.Text
+            string teksti = ViestiTB.Text;
+
+            // Sijoitetaan teksti TulostusLB.Text:in
+            TulostusLB.Text = teksti;
+
+            //TulostusLB.Text = ViestiTB.Text; //
+            // tässä vaiheessa ei tapahdu mitään vielä...
+
+            TulostusLB.Visible = true;
+        }
+    }
+}
