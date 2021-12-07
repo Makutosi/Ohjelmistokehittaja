409b50e05bdb1cdba81f5b0dccca2dc2d1a4650c
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-12-07 11:21:32 +0200

    Tehtävä 11_Nopanheittopeli
    Tehtävä 11_Nopanheittopeli

---
+++ Aronia/Viikko_6/Nopanheittopeli/Nopanheittopeli/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Nopanheittopeli
+{
+    partial class NopanHeitto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.HeitaBT = new System.Windows.Forms.Button();
+            this.noppa02PB = new System.Windows.Forms.PictureBox();
+            this.noppa01PB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa02PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa01PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nopan heitto";
+            // 
+            // HeitaBT
+            // 
+            this.HeitaBT.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeitaBT.Location = new System.Drawing.Point(32, 223);
+            this.HeitaBT.Name = "HeitaBT";
+            this.HeitaBT.Size = new System.Drawing.Size(252, 39);
+            this.HeitaBT.TabIndex = 3;
+            this.HeitaBT.Text = "Heitä";
+            this.HeitaBT.UseVisualStyleBackColor = true;
+            this.HeitaBT.Click += new System.EventHandler(this.HeitaBT_Click);
+            // 
+            // noppa02PB
+            // 
+            this.noppa02PB.Image = global::Nopanheittopeli.Properties.Resources.noppa01;
+            this.noppa02PB.Location = new System.Drawing.Point(184, 102);
+            this.noppa02PB.Name = "noppa02PB";
+            this.noppa02PB.Size = new System.Drawing.Size(100, 100);
+            this.noppa02PB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.noppa02PB.TabIndex = 2;
+            this.noppa02PB.TabStop = false;
+            // 
+            // noppa01PB
+            // 
+            this.noppa01PB.Image = global::Nopanheittopeli.Properties.Resources.noppa01;
+            this.noppa01PB.Location = new System.Drawing.Point(32, 102);
+            this.noppa01PB.Name = "noppa01PB";
+            this.noppa01PB.Size = new System.Drawing.Size(100, 100);
+            this.noppa01PB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.noppa01PB.TabIndex = 1;
+            this.noppa01PB.TabStop = false;
+            // 
+            // NopanHeitto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 293);
+            this.Controls.Add(this.HeitaBT);
+            this.Controls.Add(this.noppa02PB);
+            this.Controls.Add(this.noppa01PB);
+            this.Controls.Add(this.label1);
+            this.Name = "NopanHeitto";
+            this.Text = "Nopan heitto";
+            ((System.ComponentModel.ISupportInitialize)(this.noppa02PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa01PB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox noppa01PB;
+        private System.Windows.Forms.PictureBox noppa02PB;
+        private System.Windows.Forms.Button HeitaBT;
+    }
+}
+
---
+++ Aronia/Viikko_6/Nopanheittopeli/Nopanheittopeli/Form1.cs
@@ -0,0 +1,54 @@
+﻿// Graafinen käyttöliittymä/ Tehtävä 11_Nopan heitto/ 1.12.2021
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nopanheittopeli
+{
+    public partial class NopanHeitto : Form
+    {
+        public NopanHeitto()
+        {
+            InitializeComponent();
+        }
+
+        private void HeitaBT_Click(object sender, EventArgs e)
+        {
+            piirraNoppa(noppa01PB);
+            piirraNoppa(noppa02PB);
+        }
+
+        private void piirraNoppa(PictureBox NoppaBox)
+        {
+            Random satunnainen = new Random(); // olio satunnainen
+            int noppa = satunnainen.Next(1, 7); // arpo 1-6 välillä
+            switch(noppa)
+            {
+                case 1:
+                    NoppaBox.Image = Properties.Resources.noppa01;
+                    break;
+                case 2:
+                    NoppaBox.Image = Properties.Resources.noppa02;
+                    break;
+                case 3:
+                    NoppaBox.Image = Properties.Resources.noppa03;
+                    break;
+                case 4:
+                    NoppaBox.Image = Properties.Resources.noppa04;
+                    break;
+                case 5:
+                    NoppaBox.Image = Properties.Resources.noppa05;
+                    break;
+                case 6:
+                    NoppaBox.Image = Properties.Resources.noppa06;
+                    break;
+            }
+        }
+    }
+}
