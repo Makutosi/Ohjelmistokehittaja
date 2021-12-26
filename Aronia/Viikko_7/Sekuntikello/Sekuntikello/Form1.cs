7030a501f3b09d62d0ce65f11bac55601ef9a2af
Makutosi <80103870+Makutosi@users.noreply.github.com>
2021-12-27 00:22:35 +0200

    Tehtävä 15_Sekuntikello/ 26.12.2021
    Tehtävä 15_Sekuntikello/ 26.12.2021

---
+++ Aronia/Viikko_7/Sekuntikello/Sekuntikello/Form1.cs
@@ -0,0 +1,58 @@
+﻿// Graafinen käyttöliittymä/ Tehtävä 15_Sekuntikello/ 26.12.2021
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics; // <--
+
+namespace Sekuntikello
+{
+    public partial class SekkariForm : Form
+    {
+        private Stopwatch sekkari; // uusi olio private, joka toimii vain tämän sisällä
+        public SekkariForm()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void KaynnistaBT_Click(object sender, EventArgs e)
+        {
+            sekkari.Start();
+        }
+
+        private void LopetaBT_Click(object sender, EventArgs e)
+        {
+            sekkari.Stop();
+        }
+
+        private void TyhjennaBT_Click(object sender, EventArgs e)
+        {
+            sekkari.Reset(); 
+        }
+
+        private void SekkariForm_Load(object sender, EventArgs e)
+        {
+            sekkari = new Stopwatch();
+        }
+
+        private void AjanottoTM_Tick(object sender, EventArgs e)
+        {
+            AikaLB.Text = string.Format("{0:hh\\:mm\\:ss\\:fff}", sekkari.Elapsed);
+                          // f -> sekunnin sadas osa
+                          //sekkari.Elasped.ToString();  liian paljon desimaaleja
+        }
+    }
+}
+//BT: Käynnistä, Lopeta, Tyhjennä <- yksi klikkaus, loput kaksi BT  +Ctrl klikkaus 
+// -> Make horizontal Spacing Equal 
+// Form/ Sekuntikello/ SekkariForm
+// Label/ AikaLB
+// Button/ KaynnistaBT/ LopetaBT/ TyhjennaBT
+// Timer/ AjanottoTM/ Enabled(käytössä)True
